ed262fff793035f0c8a029c798cf52e200958086
pasandevin <jayawardene.pasandevin@gmail.com>
2022-03-25 21:09:29 +0530

    data receiver ui

--- app/src/main/java/com/pasandevin/android/profile_saver_app/MainActivity.kt
+++ app/src/main/java/com/pasandevin/android/profile_saver_app/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.pasandevin.android.profile_saver_app
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_user_data_receiver)
+
+
+
+
     }
 }
