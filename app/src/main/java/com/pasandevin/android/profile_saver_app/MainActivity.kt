fa384e19c278e20173100522a944605b6b6e2fc8
pasandevin <jayawardene.pasandevin@gmail.com>
2022-03-25 22:33:45 +0530

    onclick listeners for textfields added

--- app/src/main/java/com/pasandevin/android/profile_saver_app/MainActivity.kt
+++ app/src/main/java/com/pasandevin/android/profile_saver_app/MainActivity.kt
@@ -7,15 +7,41 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.pasandevin.android.profile_saver_app.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_user_data_receiver)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.registerbutton.setOnClickListener { view ->
+            this.goToSecondActivity()
+        }
+
+        binding.textPersonName.setOnClickListener {
+            binding.textPersonName.text = null
+        }
+
+        binding.textemail.setOnClickListener {
+            binding.textemail.text = null
+        }
+
+        binding.textphone.setOnClickListener {
+            binding.textphone.text = null
+        }
 
 
+    }
 
-
+    fun goToSecondActivity() {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/pasandevin/android/profile_saver_app/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.pasandevin.android.profile_saver_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
